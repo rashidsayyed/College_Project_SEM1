d48b39ba20b85d0bf141722b77f00694ea827021
rashidsayyed <rashidsayyed.stud@vivacollege.org>
2023-12-07 22:59:59 +0530

    Added the base files with FE

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 999 999 999</label>
+            <i className='fa fa-envelope'></i>
+            <label> support@AR2store.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <label>INDIA</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
